7b3a1efe216a2795a5cc91eeece59bafd1c720b7
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2025-11-27 10:59:38 +0530

    Update S3 bucket name for uniqueness

--- s3.tf
+++ s3.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "test_bucket" {
-  bucket = "my-test-bucket-132423322345"  # Make sure this bucket name is globally unique
+  bucket = "my-test-bucket-132423322312145"  # Make sure this bucket name is globally unique
   acl    = "private"  # Setting the bucket ACL to private
 }
